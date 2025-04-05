88201aa85624c5105b9601605be4bc8980afc4b4
mdimado <mdimad005@gmail.com>
2025-04-05 22:40:34 +0530

    comments

--- projects/goipp/oss_fuzz_build.sh
+++ projects/goipp/oss_fuzz_build.sh
@@ -1,22 +1,21 @@
 #!/bin/bash -eu
 
-# Copy the fuzzer code to the goipp package directory
+#copy fuzzer code to goipp directory
 mkdir -p $SRC/goipp/fuzzer
 cp $SRC/fuzzing/projects/goipp/fuzzer/fuzz_decode_bytes.go $SRC/goipp/fuzzer/
 
-# Prepare corpus directory
+#prepare corpus directory
 mkdir -p $WORK/corpus
 cp $SRC/fuzzing/projects/goipp/seeds/* $WORK/corpus/
 
-# Compress corpus into a zip file
+#compress corpus into zip file
 cd $WORK
 zip -r $OUT/fuzz_decode_bytes_seed_corpus.zip corpus/
 
-# Build the fuzzer with Go114 fuzzing engine
 cd $SRC/goipp
 go mod tidy
 
-# Build the fuzz target 
+#build fuzz target 
 compile_go_fuzzer github.com/OpenPrinting/goipp/fuzzer FuzzDecodeBytes fuzz_decode_bytes
 
 
