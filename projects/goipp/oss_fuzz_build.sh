5ee2d2c4d325bfc42694fe0dc137ff7ca29aef3a
Mohammed Imaduddin <mdimad005@gmail.com>
2025-04-05 19:12:10 +0530

    create go module in fuzzer folder

--- projects/goipp/oss_fuzz_build.sh
+++ projects/goipp/oss_fuzz_build.sh
@@ -6,6 +6,10 @@ cd $SRC/goipp
 # Install go-118-fuzz-build
 go install github.com/AdamKorcz/go-118-fuzz-build@latest
 
+cd $SRC/fuzzing/projects/goipp/fuzzer
+go mod init goipp-fuzz
+go get github.com/OpenPrinting/goipp
+
 # Build the fuzz target
 compile_go_fuzzer github.com/mdimado/fuzzing/goipp/fuzzer FuzzDecodeBytes fuzzer_goipp_decodebytes
 
