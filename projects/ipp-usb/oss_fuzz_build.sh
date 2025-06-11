eb764eca1e7b367614e86025afb426d97b1ea7fb
mdimado <mdimad005@gmail.com>
2025-06-11 18:47:10 +0530

    edit build script to use local ipp-usb instead of remote

--- projects/ipp-usb/oss_fuzz_build.sh
+++ projects/ipp-usb/oss_fuzz_build.sh
@@ -13,6 +13,13 @@ zip -r $OUT/fuzz_ipp_attrs_get_strings_seed_corpus.zip ipp_attrs_seed_corpus/
 # Build dependencies and compile the fuzzers
 cd $SRC/ipp-usb
 go mod tidy
+
+# use local ipp-usb instead of remote
+cd $SRC/fuzzing/projects/ipp-usb/fuzzer
+go mod init fuzzers.local/ippusb
+go mod tidy
+go mod edit -replace=github.com/OpenPrinting/ipp-usb=$SRC/ipp-usb
+
 go install github.com/AdamKorcz/go-118-fuzz-build@latest
 go get github.com/AdamKorcz/go-118-fuzz-build/testing
 
