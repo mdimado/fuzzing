3e9790922f172312acd8e492fb68d8bf64d1e868
mdimado <mdimad005@gmail.com>
2025-06-20 00:12:01 +0530

    copy over go.mod and go.sum

--- projects/ipp-usb/oss_fuzz_build.sh
+++ projects/ipp-usb/oss_fuzz_build.sh
@@ -4,6 +4,15 @@
 mkdir -p $SRC/ipp-usb/fuzzer
 cp $SRC/fuzzing/projects/ipp-usb/fuzzer/*.go $SRC/ipp-usb/fuzzer/
 
+
+# use local ipp-usb instead of remote
+cp $SRC/fuzzing/projects/ipp-usb/ipp-usb-local/go.mod $SRC/ipp-usb/go.mod || true
+cp $SRC/fuzzing/projects/ipp-usb/ipp-usb-local/go.sum $SRC/ipp-usb/go.sum || true
+cd $SRC/ipp-usb
+go mod edit -replace=github.com/OpenPrinting/ipp-usb=$SRC/ipp-usb
+go mod tidy
+
+
 # Prepare seed corpus
 mkdir -p $WORK/ipp_attrs_seed_corpus
 cp $SRC/fuzzing/projects/ipp-usb/seeds/ipp_attrs_seed_corpus/* $WORK/ipp_attrs_seed_corpus/
@@ -12,11 +21,6 @@ zip -r $OUT/fuzz_ipp_attrs_get_strings_seed_corpus.zip ipp_attrs_seed_corpus/
 
 # Build dependencies and compile the fuzzers
 cd $SRC/ipp-usb
-
-# use local ipp-usb instead of remote
-go mod init github.com/OpenPrinting/ipp-usb || true
-go mod edit -replace=github.com/OpenPrinting/ipp-usb=$SRC/ipp-usb
-go mod tidy
 
 go install github.com/AdamKorcz/go-118-fuzz-build@latest
 go get github.com/AdamKorcz/go-118-fuzz-build/testing
