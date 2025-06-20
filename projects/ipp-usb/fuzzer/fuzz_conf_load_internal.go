d4fc9d2faed2f9b49d8ca8c6249b9ba0e977df6d
mdimado <mdimad005@gmail.com>
2025-06-20 23:21:22 +0530

    add fuzzer conf load

---
+++ projects/ipp-usb/fuzzer/fuzz_conf_load_internal.go
@@ -0,0 +1,54 @@
+package fuzz
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func FuzzConfLoadInternal(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		if len(data) == 0 {
+			t.Skip()
+		}
+
+		tmpDir, err := os.MkdirTemp("", "ipp-usb-fuzz")
+		if err != nil {
+			t.Skip()
+		}
+		defer os.RemoveAll(tmpDir)
+
+		confFile := filepath.Join(tmpDir, "ipp-usb.conf")
+		err = os.WriteFile(confFile, data, 0644)
+		if err != nil {
+			t.Skip()
+		}
+
+		// Backup and reset config
+		origConf := Conf
+		defer func() { Conf = origConf }()
+		Conf = Configuration{
+			HTTPMinPort:        60000,
+			HTTPMaxPort:        65535,
+			DNSSdEnable:        true,
+			LoopbackOnly:       true,
+			IPV6Enable:         true,
+			ConfAuthUID:        nil,
+			LogDevice:          LogDebug,
+			LogMain:            LogDebug,
+			LogConsole:         LogDebug,
+			LogMaxFileSize:     256 * 1024,
+			LogMaxBackupFiles:  5,
+			LogAllPrinterAttrs: false,
+			ColorConsole:       true,
+		}
+
+		defer func() {
+			if r := recover(); r != nil {
+				t.Errorf("panic: %v", r)
+			}
+		}()
+
+		_ = confLoadInternal(confFile)
+	})
+}
--- projects/ipp-usb/oss_fuzz_build.sh
+++ projects/ipp-usb/oss_fuzz_build.sh
@@ -16,10 +16,10 @@ go mod tidy
 
 
 # Prepare seed corpus
-mkdir -p $WORK/ipp_attrs_seed_corpus
-cp $SRC/fuzzing/projects/ipp-usb/seeds/ipp_attrs_seed_corpus/* $WORK/ipp_attrs_seed_corpus/
-cd $WORK
-zip -r $OUT/fuzz_ipp_attrs_get_strings_seed_corpus.zip ipp_attrs_seed_corpus/
+# mkdir -p $WORK/ipp_attrs_seed_corpus
+# cp $SRC/fuzzing/projects/ipp-usb/seeds/ipp_attrs_seed_corpus/* $WORK/ipp_attrs_seed_corpus/
+# cd $WORK
+# zip -r $OUT/fuzz_ipp_attrs_get_strings_seed_corpus.zip ipp_attrs_seed_corpus/
 
 # Build dependencies and compile the fuzzers
 cd $SRC/ipp-usb
@@ -28,4 +28,4 @@ go install github.com/AdamKorcz/go-118-fuzz-build@latest
 go get github.com/AdamKorcz/go-118-fuzz-build/testing
 
 # Compile fuzzers
-compile_native_go_fuzzer ./fuzzer FuzzIppAttrsGetStrings fuzz_ipp_attrs_get_strings
+compile_native_go_fuzzer ./fuzzer FuzzConfLoadInternal fuzz_conf_load_internal
