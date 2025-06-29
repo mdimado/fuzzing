b0e900c30d96cd664e4e45ef34ba1a601a94f614
mdimado <mdimad005@gmail.com>
2025-06-29 22:43:29 +0530

    initialize ipp-usb

---
+++ projects/ipp-usb/fuzzer/fuzz_ipp_sanitization.go
@@ -0,0 +1,28 @@
+package fuzz
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"testing"
+)
+
+func FuzzIPPSanitization(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		// mock HTTP response with IPP content
+		resp := &http.Response{
+			Header:        make(http.Header),
+			Body:          io.NopCloser(bytes.NewReader(data)),
+			ContentLength: int64(len(data)),
+		}
+		resp.Header.Set("Content-Type", "application/ipp")
+
+		transport := &UsbTransport{
+			log: NewLogger(),
+		}
+		transport.log.ToNowhere()
+
+		// Testing the sanitization function
+		transport.sanitizeIppResponse(1, resp)
+	})
+}
---
+++ projects/ipp-usb/oss_fuzz_build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -eu
+
+mkdir -p $SRC/ipp-usb/fuzzer
+
+cp $SRC/fuzzing/projects/ipp-usb/fuzzer/fuzz_ipp_sanitization.go $SRC/ipp-usb/fuzzer/
+
+# Prepare the seed corpus
+mkdir -p $WORK/ipp_sanitization_seed_corpus
+cp $SRC/fuzzing/projects/ipp-usb/seeds/ipp_sanitization_seed_corpus/* $WORK/ipp_sanitization_seed_corpus/
+cd $WORK
+zip -r $OUT/FuzzIPPSanitization_seed_corpus.zip ipp_sanitization_seed_corpus/
+
+
+cd $SRC/ipp-usb
+go mod tidy
+go install github.com/AdamKorcz/go-118-fuzz-build@latest
+go get github.com/AdamKorcz/go-118-fuzz-build/testing
+
+# Compile fuzzers
+compile_native_go_fuzzer ./fuzzer FuzzIPPSanitization fuzz_ipp_sanitization
