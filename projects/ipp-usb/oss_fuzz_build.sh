83170123b6b4089a92b0fbc29bd12f7421185cff
mdimado <mdimad005@gmail.com>
2025-08-10 21:38:05 +0530

    change build

--- projects/ipp-usb/oss_fuzz_build.sh
+++ projects/ipp-usb/oss_fuzz_build.sh
@@ -5,17 +5,24 @@ cd $SRC/ipp-usb
 
 echo "Building ipp-usb fuzzers..."
 
+# Update go.mod to use a newer Go version that supports fuzzing
+go mod edit -go=1.18
+
+# Enable module mode and allow module updates
+export GO111MODULE=on
+go mod tidy
+
 # Compile fuzzers using Go's built-in fuzzing support for libFuzzer
 # This creates libFuzzer-compatible binaries from Go fuzz functions
 
 # Build the main HTTP fuzzer
-compile_native_go_fuzzer github.com/OpenPrinting/ipp-usb/fuzzer FuzzHTTPInterface fuzz_ipp_usb_http
+compile_native_go_fuzzer . FuzzHTTPInterface fuzz_ipp_usb_http
 
 # Build the IPP message fuzzer  
-compile_native_go_fuzzer github.com/OpenPrinting/ipp-usb/fuzzer FuzzIPPMessage fuzz_ipp_usb_message
+compile_native_go_fuzzer . FuzzIPPMessage fuzz_ipp_usb_message
 
 # Build the USB descriptor fuzzer
-compile_native_go_fuzzer github.com/OpenPrinting/ipp-usb/fuzzer FuzzUSBDescriptor fuzz_ipp_usb_descriptor
+compile_native_go_fuzzer . FuzzUSBDescriptor fuzz_ipp_usb_descriptor
 
 # Copy seed corpora if they exist
 echo "Copying seed corpora..."
