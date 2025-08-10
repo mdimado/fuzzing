1e893bb44f008d1d33153b0e901fbab51fa29a8a
mdimado <mdimad005@gmail.com>
2025-08-10 22:25:42 +0530

    change build

--- projects/ipp-usb/oss_fuzz_build.sh
+++ projects/ipp-usb/oss_fuzz_build.sh
@@ -8,9 +8,19 @@ echo "Building ipp-usb fuzzers..."
 # Use readonly mode to respect existing vendor directory
 export GOFLAGS="-mod=readonly"
 
+# Find the correct fuzzer path
+if [ -d "./fuzzer" ]; then
+    FUZZER_PATH="./fuzzer"
+elif [ -d "/src/fuzzing/projects/ipp-usb/fuzzer" ]; then
+    FUZZER_PATH="/src/fuzzing/projects/ipp-usb/fuzzer"
+else
+    # Put fuzzer in current directory
+    FUZZER_PATH="."
+fi
+
 # Build fuzzers using compile_native_go_fuzzer
-compile_native_go_fuzzer ./fuzzer FuzzHTTPInterface fuzz_ipp_usb_http
-compile_native_go_fuzzer ./fuzzer FuzzIPPMessage fuzz_ipp_usb_message
-compile_native_go_fuzzer ./fuzzer FuzzUSBDescriptor fuzz_ipp_usb_descriptor
+compile_native_go_fuzzer $FUZZER_PATH FuzzHTTPInterface fuzz_ipp_usb_http
+compile_native_go_fuzzer $FUZZER_PATH FuzzIPPMessage fuzz_ipp_usb_message
+compile_native_go_fuzzer $FUZZER_PATH FuzzUSBDescriptor fuzz_ipp_usb_descriptor
 
 echo "ipp-usb fuzzers built successfully"
